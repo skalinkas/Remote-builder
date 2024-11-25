db974ed4514a61877d6ea30b11d9801bdb0b3a2a
Timothy Demaegdt <Timothy.demaegdt@pinscher.pro>
2024-11-25 19:19:40 +0100

    update install

--- custom_components/esp32_view_creator/__init__.py
+++ custom_components/esp32_view_creator/__init__.py
@@ -1,7 +1,19 @@
-async def async_setup(hass, config):
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "esp32_view_creator"
+
+async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the ESP32 View Creator component."""
     return True
 
-async def async_setup_entry(hass, config_entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up ESP32 View Creator from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload an ESP32 View Creator config entry."""
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
--- custom_components/esp32_view_creator/config_flow.py
+++ custom_components/esp32_view_creator/config_flow.py
@@ -1,7 +1,24 @@
 from homeassistant import config_entries
+from homeassistant.const import CONF_HOST, CONF_PORT
+import voluptuous as vol
 
-class ESP32ViewCreatorConfigFlow(config_entries.ConfigFlow, domain="esp32_view_creator"):
+DOMAIN = "esp32_view_creator"
+
+class ESP32ViewCreatorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """ESP32 View Creator config flow."""
+
     async def async_step_user(self, user_input=None):
-        """Handle user setup."""
-        return self.async_create_entry(title="ESP32 View Creator", data={})
+        """Handle the initial step."""
+        if user_input is not None:
+            return self.async_create_entry(title="ESP32 View Creator", data=user_input)
+
+        # Show a form for entering the configuration
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_HOST): str,
+                    vol.Optional(CONF_PORT, default=80): int,
+                }
+            ),
+        )
